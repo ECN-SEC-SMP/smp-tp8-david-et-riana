7d5ca3e1fe41ad2f6eca42373d88c92324087f81
David Prospérin <32713756+dprosperin@users.noreply.github.com>
2026-03-09 10:38:11 +0100

    Implémentation des méthodes deux contructeurs, getNom, getX, getY, getVivant, getAttaque, setNom et setVivant

--- animaux.cpp
+++ animaux.cpp
@@ -37,7 +37,56 @@ ostream& operator<<(ostream &os, const attaque_e &a){
     return os;
 }
 
+/**
+ * Contructeur de la classe Animal
+ * Place l'animal à la position (0, 0) sur le plateau de jeu
+ * @param maxX Taille maximale de l'aire de jeu en x
+ * @param maxY Taille maximale de l'aire de jeu en y
+ */
+Animal::Animal(int maxX, int maxY) {
+    vivant_ = true;
+    x_ = 0;
+    y_ = 0;
+}
 
+/**
+ * Contructeur de la classe Animal
+ * Place l'animal à la position (a, b) sur le plateau de jeu
+ * @param maxX Taille maximale de l'aire de jeu en x
+ * @param maxY Taille maximale de l'aire de jeu en y
+ * @param a Position en x de l'animal
+ * @param b Position en y de l'animal
+ */
+Animal::Animal(int maxX, int maxY, int a, int b) {
+    vivant_ = true;
+    x_ = a;
+    y_ = b;
+}
 
+string Animal::getNom() const {
+    return nom_;
+}
 
+int Animal::getX() const {
+    return x_;
+}
 
+int Animal::getY() const {
+    return y_;
+}
+
+bool Animal::getVivant() const {
+    return vivant_;
+}
+
+Attaque Animal::getAttaque() const {
+    return type_attaque_;
+}
+
+void Animal::setNom(string nom) {
+    nom_ = nom;
+}
+
+void Animal::setVivant(bool v) {
+    vivant_ = v;
+}
